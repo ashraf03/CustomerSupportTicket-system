b3e99ba52518c8eb229477435429b46c9ae634a5
ashraf03 <eng.ashrafuddin.lus@gmail.com>
2026-03-05 09:58:02 +0600

    Added mainsection, footer in app.js

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='grid grid-cols-4 w-[1200px] mx-auto mt-10'>
+            <div>
+                <h3>CS — Ticket System</h3>
+            </div>
+            <div>
+                <h3>CS — Ticket System</h3>
+            </div>
+            <div>
+                <h3>CS — Ticket System</h3>
+            </div>
+            <div>
+                <h3>CS — Ticket System</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
