856fc458c9065b113d48b9b2c1bbc239b17211ad
ashraf03 <eng.ashrafuddin.lus@gmail.com>
2026-03-08 09:59:48 +0600

    Upadted

--- src/components/MainSection/TaskStatus/TaskStatus.jsx
+++ src/components/MainSection/TaskStatus/TaskStatus.jsx
@@ -6,7 +6,14 @@ const TaskStatus = ({tiket}) => {
         <div className='w-[338px]'>
             <h3 className=''>Task Status</h3>
             
-                
+                {
+                    tiket.map(task => (
+                        <div>
+                            <h1>{task.title}</h1>
+                            <button className="bg-[#02A53B] p-3 w-full">Complete</button>
+                        </div>
+                    ))
+                }
             
         </div>
     );
